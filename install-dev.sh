53593e0a099474e60ce80e4e924ffd9ace43992e
CallumJHays <callumjhays@gmail.com>
2020-12-30 22:03:37 +1000

    fix dev setup

--- install-dev.sh
+++ install-dev.sh
@@ -4,8 +4,8 @@ set -o xtrace # display commands
 set -e # crash on any error
 
 # installs most packages relevant to bdsim development on an OS (in editable mode)
-# the order is important so that deps are resolved locally
-for PKG in core matplotlib micropython realtime robots server webapp-full server sim
+# the order is important so that deps are resolved locally where possible
+for PKG in core matplotlib micropython realtime robots webapp-full server sim webapp-lite vision
 do
-  pip install --no-index -e pkgs/$PKG
+  pip install -e pkgs/$PKG
 done
--- pkgs/vision/setup.py
+++ pkgs/vision/setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_namespace_packages
-from shared_setup import setup_pkg
+import sys
+sys.path.append('../../')
+from shared_setup import setup_pkg  # nopep8
 
 setup_pkg(
     name='bdsim.vision',
     packages=find_namespace_packages(),
     description='OpenCV blocks for bdsim',
-    install_requires=['bdsim.core', 'opencv']
+    install_requires=['bdsim.core', 'opencv-python']
 )
